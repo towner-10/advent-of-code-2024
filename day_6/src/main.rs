7126c2587105b5cb3efd48c5f143fee21e9247c2
Collin Town <collintown@gmail.com>
2024-12-07 23:57:07 -0500

    feat: still working on day 6

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -1,24 +1,136 @@
-use std::{env, fs};
+use std::{collections::HashSet, env, fs, hash::Hash, ops};
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 struct Point {
-    x: u32,
-    y: u32,
+    x: i32,
+    y: i32,
 }
 
 #[derive(Debug, Clone, Copy)]
 struct GridPoint {
     traversed: bool,
+    infinite_loop: bool,
     obstacle: bool,
-    possible_infinite_loop: bool,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 enum Direction {
     Up,
     Down,
     Left,
     Right,
+}
+
+trait DirectionTrait {
+    fn rotate_clockwise(&self) -> Direction;
+}
+
+impl DirectionTrait for Direction {
+    fn rotate_clockwise(&self) -> Direction {
+        match self {
+            Direction::Up => Direction::Right,
+            Direction::Right => Direction::Down,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+        }
+    }
+}
+
+trait PointTrait {
+    fn move_direction(&self, direction: &Direction) -> Point;
+    fn move_opposite(&self, direction: &Direction) -> Point;
+}
+
+impl PointTrait for Point {
+    fn move_direction(&self, direction: &Direction) -> Point {
+        match direction {
+            Direction::Up => Point {
+                x: self.x,
+                y: self.y - 1,
+            },
+            Direction::Down => Point {
+                x: self.x,
+                y: self.y + 1,
+            },
+            Direction::Left => Point {
+                x: self.x - 1,
+                y: self.y,
+            },
+            Direction::Right => Point {
+                x: self.x + 1,
+                y: self.y,
+            },
+        }
+    }
+
+    fn move_opposite(&self, direction: &Direction) -> Point {
+        match direction {
+            Direction::Up => Point {
+                x: self.x,
+                y: self.y + 1,
+            },
+            Direction::Down => Point {
+                x: self.x,
+                y: self.y - 1,
+            },
+            Direction::Left => Point {
+                x: self.x + 1,
+                y: self.y,
+            },
+            Direction::Right => Point {
+                x: self.x - 1,
+                y: self.y,
+            },
+        }
+    }
+}
+
+impl ops::Add<Direction> for Point {
+    type Output = Point;
+
+    fn add(self, rhs: Direction) -> Point {
+        self.move_direction(&rhs)
+    }
+}
+
+impl ops::Sub<Direction> for Point {
+    type Output = Point;
+
+    fn sub(self, rhs: Direction) -> Point {
+        self.move_opposite(&rhs)
+    }
+}
+
+fn grid_contains(grid: &Vec<Vec<GridPoint>>, point: Point) -> bool {
+    point.x < grid[0].len() as i32 && point.y < grid.len() as i32 && point.x >= 0 && point.y >= 0
+}
+
+// If it returns to the same point and direction, it's an infinite loop
+fn has_cycle(grid: &Vec<Vec<GridPoint>>, point: Point, direction: Direction) -> bool {
+    let mut seen: HashSet<(Point, Direction)> = HashSet::new();
+    seen.insert((point, direction));
+
+    let mut current = point;
+    let mut current_direction = direction;
+
+    while grid_contains(grid, current + current_direction) {
+        let next = current + current_direction;
+
+        if grid[next.y as usize][next.x as usize].obstacle {
+            current_direction = current_direction.rotate_clockwise();
+            continue;
+        }
+
+        if seen.contains(&(next, current_direction)) {
+            return true;
+        }
+
+        seen.insert((next, current_direction));
+
+        current = next;
+    }
+
+    false
 }
 
 fn main() {
@@ -53,50 +165,23 @@ fn main() {
                     row.push(GridPoint {
                         traversed: true,
                         obstacle: false,
-                        possible_infinite_loop: false,
+                        infinite_loop: false,
                     });
                     start = Point { x, y };
                     direction = Direction::Up;
-                }
-                'v' => {
-                    row.push(GridPoint {
-                        traversed: true,
-                        obstacle: false,
-                        possible_infinite_loop: false,
-                    });
-                    start = Point { x, y };
-                    direction = Direction::Down;
-                }
-                '<' => {
-                    row.push(GridPoint {
-                        traversed: true,
-                        obstacle: false,
-                        possible_infinite_loop: false,
-                    });
-                    start = Point { x, y };
-                    direction = Direction::Left;
-                }
-                '>' => {
-                    row.push(GridPoint {
-                        traversed: true,
-                        obstacle: false,
-                        possible_infinite_loop: false,
-                    });
-                    start = Point { x, y };
-                    direction = Direction::Right;
                 }
                 '#' => {
                     row.push(GridPoint {
                         traversed: false,
                         obstacle: true,
-                        possible_infinite_loop: false,
+                        infinite_loop: false,
                     });
                 }
                 '.' => {
                     row.push(GridPoint {
                         traversed: false,
                         obstacle: false,
-                        possible_infinite_loop: false,
+                        infinite_loop: false,
                     });
                 }
                 _ => {
@@ -115,124 +200,37 @@ fn main() {
 
     let mut current = start;
 
-    loop {
-        let mut hit_edge = false;
+    while grid_contains(&grid, current + direction) {
+        let next = current + direction;
 
-        match direction {
-            Direction::Up => {
-                if current.y == 0 {
-                    hit_edge = true;
-                } else {
-                    current.y -= 1;
-
-                    if current.x < grid[current.y as usize].len() as u32
-                        && grid[current.y as usize][(current.x + 1) as usize].traversed
-                    {
-                        grid[current.y as usize][current.x as usize].possible_infinite_loop = true;
-                    }
-                }
-            }
-            Direction::Down => {
-                if current.y == grid.len() as u32 - 1 {
-                    hit_edge = true;
-                } else {
-                    current.y += 1;
-
-                    if current.x < grid[current.y as usize].len() as u32
-                        && grid[current.y as usize][(current.x - 1) as usize].traversed
-                    {
-                        grid[current.y as usize][current.x as usize].possible_infinite_loop = true;
-                    }
-                }
-            }
-            Direction::Left => {
-                if current.x == 0 {
-                    hit_edge = true;
-                } else {
-                    current.x -= 1;
-
-                    if current.y < grid.len() as u32
-                        && grid[(current.y - 1) as usize][current.x as usize].traversed
-                    {
-                        grid[current.y as usize][current.x as usize].possible_infinite_loop = true;
-                    }
-                }
-            }
-            Direction::Right => {
-                if current.x == grid[0].len() as u32 - 1 {
-                    hit_edge = true;
-                } else {
-                    current.x += 1;
-
-                    if current.y < grid.len() as u32
-                        && grid[(current.y + 1) as usize][current.x as usize].traversed
-                    {
-                        grid[current.y as usize][current.x as usize].possible_infinite_loop = true;
-                    }
-                }
-            }
+        if grid[next.y as usize][next.x as usize].obstacle {
+            direction = direction.rotate_clockwise();
+            continue;
         }
 
-        // Check if the next point is an obstacle
-        if grid[current.y as usize][current.x as usize].obstacle {
-            // Move back
-            match direction {
-                Direction::Up => {
-                    current.y += 1;
-                }
-                Direction::Down => {
-                    current.y -= 1;
-                }
-                Direction::Left => {
-                    current.x += 1;
-                }
-                Direction::Right => {
-                    current.x -= 1;
-                }
-            }
-
-            match direction {
-                Direction::Up => {
-                    direction = Direction::Right;
-                }
-                Direction::Down => {
-                    direction = Direction::Left;
-                }
-                Direction::Left => {
-                    direction = Direction::Up;
-                }
-                Direction::Right => {
-                    direction = Direction::Down;
-                }
-            }
-        } else {
-            grid[current.y as usize][current.x as usize].traversed = true;
+        grid[next.y as usize][next.x as usize].obstacle = true;
+        if !grid[next.y as usize][next.x as usize].infinite_loop {
+            let cycle = has_cycle(&grid, current, direction);
+            grid[next.y as usize][next.x as usize].infinite_loop = cycle;
         }
+        grid[next.y as usize][next.x as usize].obstacle = false;
 
-        if hit_edge {
-            break;
-        }
+        grid[current.y as usize][current.x as usize].traversed = true;
+        current = next;
     }
 
-    let mut part_1 = 0;
+    let mut part_1 = 1;
     let mut part_2 = 0;
 
     for row in &grid {
         for point in row {
             if point.traversed {
-                print!("X");
                 part_1 += 1;
-            } else if point.possible_infinite_loop {
-                print!("O");
+            }
+            if point.infinite_loop {
                 part_2 += 1;
-            } else if point.obstacle {
-                print!("#");
-            } else {
-                print!(".");
             }
         }
-        println!();
-
     }
 
     println!("Part 1: {}", part_1);
